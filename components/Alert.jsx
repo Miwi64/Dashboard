908a13a1219e4d11498f5d89501308e5b8bbfc84
Miguel <miargote@hotmail.com>
2023-09-07 01:52:18 -0700

    Agregados parametros

--- components/Alert.jsx
+++ components/Alert.jsx
@@ -1,7 +1,7 @@
-function Alert(){
+function Alert({text}){
     return(
         <div className='p-5 bg-boxback border-2 border-line rounded-xl text-center mt-4'>
-            <p className={`text-2xl text-white`}>Llenar todos los campos</p>
+            <p className={`text-2xl text-white`}>{text}</p>
         </div>
     )
   }
